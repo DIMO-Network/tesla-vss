6b79541dfd62361e5505039bcd3d63c5c27e5318
Dylan Moreland <dylan@dimo.zone>
2025-03-03 18:01:50 -0500

    Describe the helper

--- pkg/convert/helpers.go
+++ pkg/convert/helpers.go
@@ -1,5 +1,8 @@
 package convert
 
+// windowStateToIsOpen converts the Tesla WindowState enum, which we typically receive
+// as a string, to 1 (open) or 0 (closed).
+// See https://github.com/teslamotors/fleet-telemetry/blob/646fce2fb2ddd607ce4e76c865ce411e32ded81f/protos/vehicle_data.proto#L465
 func windowStateToIsOpen(state string) float64 {
 	if state == "PartiallyOpen" || state == "Opened" {
 		return 1
